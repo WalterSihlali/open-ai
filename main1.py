eb860c8ef8daa267d0237c212eb1a3acaf6fb583
WalterSihlali <sihlali.walter@gmail.com>
2024-03-14 12:34:38 +0200

    First commit

---
+++ main1.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+
+
+client = OpenAI(
+  api_key="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"
+)
+
+
+def chat_with_gtp(prompt):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role":"user", "content":prompt}]
+
+    )
+
+    return response.choices[0].message.content.split()
+
+
+if __name__ == "__main__":
+     while True:
+         user_input= input("User: ")
+         if user_input.lower() in ["quit", "exit", "bye"]:
+             break
+
+         response = chat_with_gtp(user_input)
+         print("Chatbot: ", response)
+
